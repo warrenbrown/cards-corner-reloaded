04494ab752c5026c3e46ecf50247f198cb3e6826
Warren Brown <warren@katapultmedia.com>
2021-09-05 11:38:32 -0500

    chore: added Header component and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TopBar from "./components/TopBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      HomePage
+    </div>
+  );
+};
+
+export default Home;
