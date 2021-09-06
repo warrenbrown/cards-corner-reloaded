aa78322c83d2012cc647b38b3333a3c8a35167d6
Warren Brown <warren@katapultmedia.com>
2021-09-06 09:01:08 -0500

    chore: added write page with styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import TopBar from "./components/TopBar";
 import Home from "./pages/Home";
 import Single from "./pages/Single";
+import Write from "./pages/Write";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
       {/* <Home /> */}
-      <Single />
+      {/* <Single /> */}
+      <Write />
     </div>
   );
 }
