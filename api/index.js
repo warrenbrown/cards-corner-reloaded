7cf7456fec0f347eac8cbc60f9f3d7713100517b
Warren Brown <warren@katapultmedia.com>
2021-09-08 15:58:37 -0500

    chore: added update route for user model

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running on Port:5000");
---
+++ api/routes/users.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("You do not have access to this account");
+  }
+});
+
+module.exports = router;
+
+//DELETE
