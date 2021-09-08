22efbb657507cab6848a90abef980e8e7b7caeac
Warren Brown <warren@katapultmedia.com>
2021-09-08 15:09:18 -0500

    chore: added auth login routes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -12,9 +15,9 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-app.use("/", (req, res) => {
-  console.log("He this is main url");
-});
+
+app.use("/api/auth", authRoute);
+
 app.listen("5000", () => {
   console.log("Backend is running on Port:5000");
 });
