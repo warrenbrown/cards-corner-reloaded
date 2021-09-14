5d4eaa8622bb6cd398265893516edaba8925bba6
Warren Brown <warren@katapultmedia.com>
2021-09-14 11:07:13 -0500

    chore: updated login page and css

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,20 +1,60 @@
-import React from "react";
+import axios from "axios";
+import { useContext, useRef } from "react";
+import { Link } from "react-router-dom";
+// import { Context } from "../../context/Context";
+import { AnyType } from "../components/Post";
 import "./login.css";
 
-const Login = () => {
+export default function Login() {
+  const userRef = useRef<AnyType>();
+  const passwordRef = useRef<AnyType>();
+  // const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e: any) => {
+    // e.preventDefault();
+    // dispatch({ type: "LOGIN_START" });
+    // try {
+    //   const res = await axios.post("/auth/login", {
+    //     username: userRef.current?.value,
+    //     password: passwordRef.current?.value,
+    //   });
+    //   dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    // } catch (err) {
+    //   dispatch({ type: "LOGIN_FAILURE" });
+    // }
+  };
+
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label className="loginFormLabel">Email</label>
-        <input type="text" className="loginFormInput" />
-        <label className="loginFormLabel">Password</label>
-        <input type="password" className="loginFormInput" />
-        <button className="loginFormSubmit">Login</button>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter your username..."
+          ref={userRef}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="Enter your password..."
+          ref={passwordRef}
+        />
+        <button
+          className="loginButton"
+          type="submit"
+          // disabled={isFetching}
+        >
+          Login
+        </button>
       </form>
-      <button className="loginFormRegister">Register</button>
+      <button className="loginRegisterButton">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+      </button>
     </div>
   );
-};
-
-export default Login;
+}
