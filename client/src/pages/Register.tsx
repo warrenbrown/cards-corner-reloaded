82246528a3ed53b86e0c2ee119ebc08acdac106d
Warren Brown <warren@katapultmedia.com>
2021-09-14 09:44:28 -0500

    chore: updated register page

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,26 +1,67 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./register.css";
 
-const Register = () => {
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
-        <label className="registerFormLabel">UserName</label>
+      <form className="registerForm" onSubmit={handleSubmit}>
+        <label>Username</label>
         <input
           type="text"
-          className="registerFormInput"
-          placeholder="Enter your username"
+          className="registerInput"
+          placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <label className="registerFormLabel">Email</label>
-        <input type="text" className="registerFormInput" />
-        <label className="registerFormLabel">Password</label>
-        <input type="password" className="registerFormInput" />
-        <button className="registerFormSubmit">Register</button>
+        <label>Email</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="registerButton" type="submit">
+          Register
+        </button>
       </form>
-      <button className="registerLoginButton">Login</button>
+      <button className="registerLoginButton">
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </button>
+      {error && (
+        <span style={{ color: "red", marginTop: "10px" }}>
+          Something went wrong!
+        </span>
+      )}
     </div>
   );
-};
-
-export default Register;
+}
